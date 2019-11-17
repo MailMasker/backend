4af9eed49342dac05dc943049240327ad2c3ff86
Jonathan Sibley <jonsibley@gmail.com>
2019-11-17 16:26:49 +0100

    initial commit

---
+++ handler.ts
@@ -0,0 +1,27 @@
+import { ApolloServer, gql } from "apollo-server-lambda";
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => "Hello world!"
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+module.exports.graphql = server.createHandler({
+  // cors: {
+  //   origin: true,
+  //   credentials: true
+  // }
+});
