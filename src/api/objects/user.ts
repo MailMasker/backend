655e4e89d32bf64f8e75c60163897e87cbe41569
Jonathan Sibley <jonsibley@gmail.com>
2019-12-24 19:21:55 +0100

    verifiedEmailByID lookup
    Former-commit-id: 7a37a0fd1b53dbb36dd4e85469cc39d47e57e580
    Former-commit-id: f7c544de198e9e2f4fbed9c24e761d6de42e2a4f

---
+++ src/api/objects/user.ts
@@ -0,0 +1,38 @@
+import * as dal from "../../dal/verifiedEmailByID";
+
+import {
+  Resolver,
+  ResolversParentTypes,
+  ResolversTypes
+} from "../types.generated";
+
+import { AuthenticatedResolverContext } from "../lib/ResolverContext";
+import { AuthenticationError } from "apollo-server-express";
+import { userForID } from "../../dal/userForID";
+
+export const user: ResolversTypes["user"] = async (
+  parent,
+  args,
+  { dalContext, currentUserID },
+  info
+) => {
+  console.log("parent", parent);
+  console.log("args", args);
+  console.log("info", info);
+  if (!currentUserID) {
+    throw new AuthenticationError("Authentication required");
+  }
+  const { id, email, verifiedEmailIDs } = await userForID(
+    dalContext,
+    currentUserID
+  );
+
+  return {
+    id,
+    email,
+    verifiedEmails: () =>
+      Promise.all(
+        verifiedEmailIDs.map(id => dal.verifiedEmailByID(dalContext, id))
+      )
+  };
+};
