e30f70a719ae831858931393242dc167fb6f1399
Jonathan Sibley <jonsibley@gmail.com>
2020-03-21 22:12:04 +0100

    actually send verification email
    Former-commit-id: 39cc3d07a0f223ef117d20aa547aa299bd9ccd29
    Former-commit-id: 370d84af6fd862983cf9c35f5113f8d033c50a30

--- src/api/mutations/createVerifiedEmail.ts
+++ src/api/mutations/createVerifiedEmail.ts
@@ -4,6 +4,13 @@ import { AuthenticatedResolverContext } from "../lib/ResolverContext";
 import { NotFoundError } from "../../dal";
 import { verifiedEmailByEmail } from "../../dal/verifiedEmailByEmail";
 
+// TODO: move this into the context
+
+// Load the AWS SDK for Node.js
+var AWS = require("aws-sdk");
+// Set the region
+AWS.config.update({ region: "REGION" });
+
 export const createVerifiedEmail = async (
   parent,
   args,
@@ -38,8 +45,48 @@ export const createVerifiedEmail = async (
     );
   }
 
-  return await dal.createVerifiedEmail(dalContext, {
+  const response = await dal.createVerifiedEmail(dalContext, {
     email: args.email,
     userID: currentUserID
   });
+
+  // Create sendEmail params
+  const params = {
+    Destination: {
+      ToAddresses: ["jonsibley@gmail.com"]
+    },
+    Message: {
+      /* required */
+      Body: {
+        Html: {
+          Charset: "UTF-8",
+          // TODO: set up for various environments: dev and prod
+          Data: `Dear username,
+
+<a href="http://localhost:3001/verify-email/jonsibley+22@gmail.com/code/c6263648-5783-45a3-904a-e394ad88851a">Click here</a> to verify your email address (jonsibley@gmail.com).`
+        }
+      },
+      Subject: {
+        Charset: "UTF-8",
+        Data: "[1nt] Verify your email address"
+      }
+    },
+    Source: "support@1nt.email"
+  };
+
+  // Create the promise and SES service object
+  const sendPromise = new AWS.SES({ apiVersion: "2010-12-01" })
+    .sendEmail(params)
+    .promise();
+
+  // Handle promise's fulfilled/rejected states
+  await sendPromise
+    .then(function(data) {
+      console.log(data.MessageId);
+    })
+    .catch(function(err) {
+      console.error(err, err.stack);
+    });
+
+  return response;
 };
