2f9a33f6f29f23c9f0a99aa9f14aa6bc4c7010c1
Jonathan Sibley <jonsibley@gmail.com>
2020-05-15 19:38:42 +0200

    after verifying email address, send intro email to all mail masks using that address
    Former-commit-id: 379287a50de46ea1d97825a4edc8d48a1de78399
    Former-commit-id: 3762e62c4ba7e49738582c319290b2fae54cb4b2

--- src/api/mutations/verifyEmailWithCode.ts
+++ src/api/mutations/verifyEmailWithCode.ts
@@ -3,11 +3,13 @@ import * as dal from "../../dal/";
 import { AuthenticationError, UserInputError } from "apollo-server-core";
 
 import { ResolverContext } from "../lib/ResolverContext";
+import sendTransactionalEmail from "../../dal/lib/sendTransactionalEmail";
+import { userByID } from "../../dal/userByID";
 
 export const verifyEmailWithCode = async (
   parent,
   { code, email },
-  { dalContext }: ResolverContext,
+  { dalContext, ses }: ResolverContext,
   info
 ) => {
   if (!code || !email) {
@@ -29,7 +31,18 @@ export const verifyEmailWithCode = async (
 
   if (verifiedEmail.verified) {
     // Look up all routes that have this email address and send the intro email
-    // verifiedEmail.ownerUserID
+    const user = await userByID(dalContext, verifiedEmail.ownerUserID);
+    await Promise.all(
+      user.emailMaskIDs.map((emailMaskID) =>
+        dal.emailMaskByID(dalContext, emailMaskID).then((emailMask) =>
+          sendTransactionalEmail(ses, {
+            to: [`${emailMask.alias}@${emailMask.domain}`],
+            subject: `[Mail Masker] Your new Mail Mask, ${emailMask.alias}@${emailMask.domain}, is now active!`,
+            bodyHTML: `Emails received at ${emailMask.alias}@${emailMask.domain} (such as this one) will be forwarded to ${verifiedEmail.email}.`,
+          })
+        )
+      )
+    );
   }
 
   return {
