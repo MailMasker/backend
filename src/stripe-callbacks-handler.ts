283958a9d6f7a223a8c82040a3cced227841540b
Jonathan Sibley <jonsibley@gmail.com>
2020-05-26 22:56:32 +0200

    checkpoint: stripe webhooks received correctly
    Former-commit-id: 5944f1838126bf836ccffdc18741d5ceeb92b5cb
    Former-commit-id: 29318a9a716ca9ed03430e902a865abb347096ef

--- src/stripe-callbacks-handler.ts
+++ src/stripe-callbacks-handler.ts
@@ -1,25 +1,51 @@
+import Bugsnag from "@bugsnag/js";
+import BugsnagPluginExpress from "@bugsnag/plugin-express";
+import { ConfigurationServicePlaceholders } from "aws-sdk/lib/config_service_placeholders";
 import bodyParser from "body-parser";
 import express from "express";
 import serverless from "serverless-http";
 
-const stripe = require("stripe")("sk_test_y4mgUwBVREMB30AG5rHnSst200EZseO09s");
+Bugsnag.start({
+  apiKey: "3e593a7f71377ef86cf65c7cda2570db",
+  plugins: [BugsnagPluginExpress],
+  releaseStage: process.env.BUGSNAG_RELEASE_STAGE,
+});
+
+if (!process.env.STRIPE_SECRET_KEY) {
+  Bugsnag.notify("process.env.STRIPE_SECRET_KEY missing");
+}
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 // Find your endpoint's secret in your Dashboard's webhook settings
-const endpointSecret = "whsec_EwMBdVeOmAR4ACrZC28AyB94EHnvlQo8";
+const endpointSecret = process.env.STRIPE_ENDPOINT_SECRET;
+if (!endpointSecret) {
+  Bugsnag.notify("endpointSecret missing");
+}
 
 const app = express();
 
 app.post(
   "/stripe-callbacks",
   bodyParser.raw({ type: "application/json" }),
-  (request, response) => {
+  (request: express.Request, response: express.Response) => {
     const sig = request.headers["stripe-signature"];
 
     let event;
 
+    console.debug("request.headers: ");
+    console.debug(request.headers);
+
+    console.debug("request: ");
+    console.debug(request);
+
+    console.debug("endpointSecret: ");
+    console.debug(endpointSecret);
+
     try {
       event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
     } catch (err) {
+      Bugsnag.notify(err);
       return response.status(400).send(`Webhook Error: ${err.message}`);
     }
 
@@ -29,6 +55,9 @@ app.post(
 
       // Fulfill the purchase...
       console.log(session);
+    } else if (event.type === "customer.subscription.deleted") {
+      const session = event.data.object;
+      console.log("deleted subscription: ", session);
     }
 
     // Return a response to acknowledge receipt of the event
