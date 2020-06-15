ceb01c68e735b30a658c15eb8c2fc25f0b4754d6
Jonathan Sibley <jonsibley@gmail.com>
2020-06-15 17:55:20 +0200

    try to prepopulate mail mask for stripe
    Former-commit-id: 488efb002bf951390fd1c33f5d934c87b30fd089
    Former-commit-id: 6c25c8d7382cfbe117d60fe61158220ca4a7deed

--- src/api/mutations/createCheckoutSession.ts
+++ src/api/mutations/createCheckoutSession.ts
@@ -8,6 +8,8 @@ import Bugsnag from "@bugsnag/js";
 import { MutationCreateCheckoutSessionArgs } from "../types.generated";
 import { UserInputError } from "apollo-server-express";
 import { createStripeCheckoutSession } from "../../dal/createStripeCheckoutSession";
+import { emailMaskByID } from "../../dal";
+import { userByID } from "../../dal/userByID";
 
 const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
 if (!process.env.STRIPE_PRIVATE_KEY) {
@@ -29,6 +31,23 @@ export const createCheckoutSession = async (
 
   console.info("creating checkout session");
 
+  let firstMailMaskEmail = "";
+  try {
+    const user = await userByID(dalContext, currentUserID);
+    if (user.emailMaskIDs?.length > 0) {
+      const firstMailMask = await emailMaskByID(
+        dalContext,
+        user.emailMaskIDs[0]
+      );
+      if (firstMailMask) {
+        firstMailMaskEmail = `${firstMailMask.alias}.stripe@${firstMailMask.domain}`;
+      }
+    }
+  } catch (err) {
+    // An error here shouldn't block
+    Bugsnag.notify(err);
+  }
+
   try {
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
@@ -41,8 +60,7 @@ export const createCheckoutSession = async (
       mode: "subscription",
       success_url: `${process.env.WEB_APP_BASE_URL}/checkout-success?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `${process.env.WEB_APP_BASE_URL}/checkout-cancel?session_id={CHECKOUT_SESSION_ID}`,
-      // TODO: prefill with a Mail Mask
-      // customer_email: `${}`
+      customer_email: firstMailMaskEmail ?? undefined,
     });
 
     console.debug("session", session);
