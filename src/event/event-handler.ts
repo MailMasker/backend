d6ccfb799fc104f4bddb8881beee0324cabe50e0
Jonathan Sibley <jonsibley@gmail.com>
2020-06-25 12:03:02 +0200

    WIP: trying to get the pending events queue working
    Former-commit-id: 13644f6601d0102e6e1d2cd38751b869ce763bdc
    Former-commit-id: dda1af39a1382cae8ac75295e309d12bd4c41ca2

--- src/event/event-handler.ts
+++ src/event/event-handler.ts
@@ -1,3 +1,4 @@
+import AWS from "aws-sdk";
 import Bugsnag from "@bugsnag/js";
 import BugsnagPluginExpress from "@bugsnag/plugin-express";
 import { Client } from "pg";
@@ -6,6 +7,8 @@ import { config } from "dotenv";
 import crypto from "crypto";
 import express from "express";
 import serverless from "serverless-http";
+
+const sns = new AWS.SNS({ region: "us-east-1" });
 
 Bugsnag.start({
   apiKey: "3e593a7f71377ef86cf65c7cda2570db",
@@ -23,6 +26,10 @@ if (!process.env.DOT_ENV_FILE) {
 }
 config({ path: process.env.DOT_ENV_FILE });
 
+if (!process.env.PENDING_EVENTS_SNS_ARN) {
+  Bugsnag.notify("process.env.PENDING_EVENTS_SNS_ARN missing");
+}
+
 const app = express();
 
 app.post(
@@ -39,30 +46,49 @@ app.post(
       return;
     }
 
-    const client = new Client({
-      user: process.env.POSTGRES_DB_USERNAME,
-      host: process.env.POSTGRES_DB_HOST,
-      // For now, we expect username and db name to be equivalent
-      database: process.env.POSTGRES_DB_USERNAME,
-      password: process.env.POSTGRES_DB_PASSWORD,
-      port: 5432,
-    });
-    client.connect();
+    console.log(
+      `publishing event ${request.body.name} for user ${request.body.userIDHash}`
+    );
+
+    let params = {
+      Message: JSON.stringify({
+        name: request.body.name,
+        userIDHash: request.body.userIDHash,
+      }),
+      TopicArn: process.env.PENDING_EVENTS_SNS_ARN,
+    };
 
     try {
-      const text = "INSERT INTO event(name, userIDHash) VALUES($1, $2, $3)";
-      const values = [
-        request.body.name,
-        crypto
-          .createHash("md5")
-          .update(request.body.userIDHash)
-          .digest("hex"),
-      ];
-      await client.query(text, values);
+      await sns.publish(params).promise();
     } catch (err) {
-      console.log(err.stack);
+      console.log(err);
       throw err;
     }
+
+    // const client = new Client({
+    //   user: process.env.POSTGRES_DB_USERNAME,
+    //   host: process.env.POSTGRES_DB_HOST,
+    //   // For now, we expect username and db name to be equivalent
+    //   database: process.env.POSTGRES_DB_USERNAME,
+    //   password: process.env.POSTGRES_DB_PASSWORD,
+    //   port: 5432,
+    // });
+    // client.connect();
+
+    // try {
+    //   const text = "INSERT INTO event(name, userIDHash) VALUES($1, $2, $3)";
+    //   const values = [
+    //     request.body.name,
+    //     crypto
+    //       .createHash("md5")
+    //       .update(request.body.userIDHash)
+    //       .digest("hex"),
+    //   ];
+    //   await client.query(text, values);
+    // } catch (err) {
+    //   console.log(err);
+    //   throw err;
+    // }
 
     console.log("ending");
 
